0f7fe31b3b7338e3ed385896d09e05c921b4a8dc
Dmitri Finkelshtein <finkelshtein@gmail.com>
2022-05-29 07:09:10 +0300

    Remove Background for Head Picture

--- components/SiteHero.vue
+++ components/SiteHero.vue
@@ -141,6 +141,15 @@ export default {
   .under-subtitle,
   .under-subtitle strong {
     color: white;
+  &.hero:after {
+    content: '';
+    top: 0;
+    left: 0;
+    right: 0;
+    bottom: 0;
+    background: rgba(0, 0, 0, 0);
+    position: absolute;
+  }
   }
 }
 .hero-theme-light.hero {
